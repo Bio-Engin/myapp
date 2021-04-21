9b4472d7d0c2b55dc4292fd5451b7e56144df1d2
parsin <amirrezakhoainyha@gmail.com>
2021-04-21 14:30:42 -0500

    fix bug findViewById

--- app/src/main/java/com/news/myapplication/VideView.java
+++ app/src/main/java/com/news/myapplication/VideView.java
@@ -3,13 +3,19 @@ package com.news.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class VideView extends AppCompatActivity {
-    VideView video;
+    VideoView video;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vide_view);
-//        video = findViewById(R.id.video);
+        video = findViewById(R.id.video);
     }
 }
+
+//chi ro eshtebah zadam?
+// migam behet
+// ok . dastet dard nakone
+//gereftam. mamnun javad :)
--- app/src/main/java/com/news/myapplication/WebVieww.java
+++ app/src/main/java/com/news/myapplication/WebVieww.java
@@ -3,13 +3,17 @@ package com.news.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
-public class WebView extends AppCompatActivity {
+public class WebVieww extends AppCompatActivity {
     WebView web;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-//        web = findViewById(R.id.web);
+        web = findViewById(R.id.web);
+
+        //tamom???
+        //baraye video ham hamine ?
     }
 }
