6176f4f1b7092e3e153f5199fbe9d7ad3564b47b
parsin <amirrezakhoainyha@gmail.com>
2021-04-23 14:11:15 -0500

    fix bug findViewById

--- app/src/main/java/com/news/myapplication/VideView.java
+++ app/src/main/java/com/news/myapplication/VideView.java
@@ -2,9 +2,12 @@ package com.news.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
+
+import java.util.ArrayList;
 
 public class VideView extends AppCompatActivity {
     VideoView video;
@@ -12,12 +15,18 @@ public class VideView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vide_view);
-        video = findViewById(R.id.video);
-        video.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.vone);
-        MediaController controller = new MediaController(this);
-        controller.setAnchorView(video);
-        video.setMediaController(controller);
 
+        ArrayList<String> play = new ArrayList<String>();
+        play.add("android.resource://"+getPackageName()+"/"+R.raw.vone);
+        play.add("android.resource://"+getPackageName()+"/"+R.raw.vtwo);
+
+        
+//        video = findViewById(R.id.video);
+//        video.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.vone);
+//        MediaController controller = new MediaController(this);
+//        controller.setAnchorView(video);
+//        video.setMediaController(controller);
+//        MediaPlayer
     }
 }
 
