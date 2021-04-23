175a98a6b5d7aedaf741c20f4dc15a8baccb8afa
parsin <amirrezakhoainyha@gmail.com>
2021-04-23 13:15:41 -0500

    fix bug findViewById

--- app/src/main/java/com/news/myapplication/VideView.java
+++ app/src/main/java/com/news/myapplication/VideView.java
@@ -3,6 +3,7 @@ package com.news.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideView extends AppCompatActivity {
@@ -12,9 +13,11 @@ public class VideView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vide_view);
         video = findViewById(R.id.video);
-//        video.setVideoPath("file:///android_res/raw/vone.mp4");
-        video.setVideoPath("https://www.aparat.com/v/HavnF");
-        video.start();
+        video.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.vone);
+        MediaController controller = new MediaController(this);
+        controller.setAnchorView(video);
+        video.setMediaController(controller);
+
     }
 }
 
