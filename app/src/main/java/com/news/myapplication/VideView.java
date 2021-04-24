24aaa4e2f66bff45b3ba027a6ded019cc593f7e4
parsin <amirrezakhoainyha@gmail.com>
2021-04-24 03:26:27 -0500

    video bug fixed

--- app/src/main/java/com/news/myapplication/VideView.java
+++ app/src/main/java/com/news/myapplication/VideView.java
@@ -2,25 +2,53 @@ package com.news.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class VideView extends AppCompatActivity {
+
+    ArrayList<String> play = new ArrayList<String>();
+    String[] my = {"android.resource://"+getPackageName()+"/"+R.raw.vone, "android.resource://"+getPackageName()+"/"+R.raw.vtwo};
+
     VideoView video;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vide_view);
+        for (int i=0; i<my.length; i++) {
+            my[i] = String.valueOf(video);
+        }
 
-        ArrayList<String> play = new ArrayList<String>();
-        play.add("android.resource://"+getPackageName()+"/"+R.raw.vone);
-        play.add("android.resource://"+getPackageName()+"/"+R.raw.vtwo);
 
-        
+        video = findViewById(R.id.video);
+        MediaController controller = new MediaController(this);
+        controller.setAnchorView(video);
+        video.setMediaController(controller);
+
+
+
+
+
+
+//        play.add("android.resource://"+getPackageName()+"/"+R.raw.vone);
+//        play.add("android.resource://"+getPackageName()+"/"+R.raw.vtwo);
+//        for (int i=0; i<play.size(); i++) {
+//
+//        }
+//        video = findViewById(R.id.video);
+//        video.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.vone);
+//        play.add(video);
+
+//        ArrayList<String> play = new ArrayList<String>();
+//        play.add("android.resource://"+getPackageName()+"/"+R.raw.vone);
+//        play.add("android.resource://"+getPackageName()+"/"+R.raw.vtwo);
+//
+//
 //        video = findViewById(R.id.video);
 //        video.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.vone);
 //        MediaController controller = new MediaController(this);
